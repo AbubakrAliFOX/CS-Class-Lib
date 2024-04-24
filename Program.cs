8c8d4ad312c362bf4c4b539a0fd324f1787113dd
Abubakr <thefox5000@gmail.com>
2024-04-24 09:21:32 +0300

    Include class library in project

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyFirstClassLib;
 
 namespace Test_C__Class_Lib
 {
@@ -10,6 +11,10 @@ namespace Test_C__Class_Lib
     {
         static void Main(string[] args)
         {
+            clsMyMath math1 = new clsMyMath();
+            Console.WriteLine(math1.Sum(10,20,30));
         }
+
+        
     }
 }
